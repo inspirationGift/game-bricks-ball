ab86134ab39512b059fffffdf177f682412fb76a
Mark <pivonosovm@gmail.com>
2020-07-19 00:43:13 +0300

    refactored, corrected path for prod. to do - write restart app method

--- src/main/Animate.java
+++ src/main/Animate.java
@@ -1,8 +1,9 @@
 package main;
 
+
 public class Animate implements Runnable {
 
-    private BlockBreakerPanel panel;
+    private final BlockBreakerPanel panel;
 
     public Animate(BlockBreakerPanel panel) {
         this.panel = panel;
